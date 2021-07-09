138bc736b6a3c7d7400799e17c0c76ebe663792a
onkar-date <onkar101197@gmail.com>
2021-07-09 19:16:54 +0530

    routing added

--- src/app/library/shared-components/navbar/navbar.component.ts
+++ src/app/library/shared-components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { SocialMedia } from './../../../shared/constants/social.constant';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
   socialMedia = SocialMedia;
   ngOnInit(): void {
+  }
+
+  showKeyboard() {
+    this.router.navigate(['../keyboard'], { relativeTo: this.activatedRoute });
   }
 
 }
