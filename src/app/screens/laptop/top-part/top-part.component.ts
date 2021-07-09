590575ecaa00997421c14d6e86f3ab2de429f38d
onkar-date <onkar101197@gmail.com>
2021-07-09 18:29:24 +0530

    wallpaper animation added

--- src/app/screens/laptop/top-part/top-part.component.ts
+++ src/app/screens/laptop/top-part/top-part.component.ts
@@ -7,24 +7,50 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class TopPartComponent implements OnInit {
-  
+  powerOn = false;
+  canToggle = true;
   constructor() { }
-  powerOn = true;
   ngOnInit(): void {
   }
 
-  async toggle(): Promise<void> {
-    const powerBtn = document.getElementById('powerBtn');
-    powerBtn.classList.toggle('on');
+  toggle() {
+    if (this.canToggle) { // to wait for power on animation to finish
+      this.powerOn = !this.powerOn;
+      const powerBtn = document.getElementById('powerBtn');
+      powerBtn.classList.toggle('on');
+      this.toggleKeys();
+      this.toggleLogoAndWallpaper();
+    }
+  }
+
+  toggleKeys() {
     const keyBtns = document.getElementsByClassName('key');
-    const hpLogo = document.getElementsByClassName('hp-logo');
-    hpLogo[0]?.classList?.toggle('on');
-    // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < keyBtns.length; i++) {
       setTimeout(() => {
         keyBtns[i].classList.toggle('on');
       }, 1.5 * i);
     }
-    this.powerOn = !this.powerOn;
   }
+
+  toggleLogoAndWallpaper() {
+    const hpLogo = document.getElementsByClassName('hp-logo');
+    if (this.powerOn) {
+      hpLogo[0]?.classList?.toggle('on');
+      this.canToggle = false;
+      setTimeout(() => {
+        hpLogo[0]?.classList?.toggle('on');
+        this.toggleWallpaper();
+      }, 1500)
+    } else {
+      this.toggleWallpaper();
+    }
+    
+  }
+
+  toggleWallpaper() {
+    const wallpaper = document.getElementsByClassName('wallpaper');
+    wallpaper[0]?.classList?.toggle('on');
+    this.canToggle = true;
+  }
+
 }
